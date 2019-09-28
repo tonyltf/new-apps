8be8f179a72127425d61ed81402e9e21ce45289c
Tony Li <tingfung.tony@gmail.com>
2019-09-28 22:59:00 +0800

    Set up meta tag,  og tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>News App</title>
+        <meta name="description" content="A simple news app" />
+        <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-1024x1024.png" />
+        <meta property="og:title" content="News App" />
+        <meta property="og:type" content="article" />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
