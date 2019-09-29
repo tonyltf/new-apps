a3b64edef498e2d3e19cc6ca73734f94192bd429
Tony Li <tingfung.tony@gmail.com>
2019-09-29 09:15:37 +0800

    Fix header & search styles

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Search from '@bit/mui-org.material-ui-icons.search';
+import SearchIcon from '@bit/mui-org.material-ui-icons.search';
 import './Search.scss';
 
-const Searchbox: React.FC = () => {
+const Search: React.FC = () => {
   return (
     <span className="news-app-search">
-      <Search />
+      <SearchIcon />
       <input type="text" placeholder="Search" className="news-app-search-input" />
     </span>
   );
 };
 
-export default Searchbox;
+export default Search;
