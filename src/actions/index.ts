a1787c1b30a1db9af1b2f275e2bfc37b9193631c
Tony Li <tony.li@mobigator.com>
2019-09-30 12:50:30 +0800

    Add success and failed

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,17 +1,41 @@
 import { News } from '../store/news';
 
 export const FETCH_NEWS = 'FETCH_NEWS';
+export const FETCH_NEWS_SUCCESS = 'FETCH_NEWS_SUCCESS';
+export const FETCH_NEWS_FAILED = 'FETCH_NEWS_FAILED';
 
 interface FetchNewsAction {
   type: typeof FETCH_NEWS;
+  loading: boolean;
+}
+interface FetchNewsSuccessAction {
+  type: typeof FETCH_NEWS_SUCCESS;
   payload: News[];
 }
+interface FetchNewsFailedAction {
+  type: typeof FETCH_NEWS_FAILED;
+  message: string;
+}
 
-export type NewsActionType = FetchNewsAction;
+export type NewsActionType = FetchNewsAction | FetchNewsSuccessAction | FetchNewsFailedAction;
 
-export function fetchNews(news: News[]): FetchNewsAction {
+export function fetchNews(loading: boolean): FetchNewsAction {
   return {
     type: FETCH_NEWS,
+    loading,
+  };
+}
+
+export function fetchNewsSuccess(news: News[]): FetchNewsSuccessAction {
+  return {
+    type: FETCH_NEWS_SUCCESS,
     payload: news,
+  };
+}
+
+export function fetchNewsFailed(message: string): FetchNewsFailedAction {
+  return {
+    type: FETCH_NEWS_FAILED,
+    message,
   };
 }
